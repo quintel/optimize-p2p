9ac8e1d454e869b3fbfa93934604836f137919ef
Anthony Williams <hi@antw.dev>
2021-09-29 14:39:34 +0100

    Add optimization script and example files

---
+++ README.md
@@ -0,0 +1,137 @@
+P2P Optimization
+================
+
+**optimize.py** receives a residual load curve and, using a battery, attempts to flatten the curve as much as possible.
+
+Two arguments are required:
+
+1. A path to a file containing the residual load.
+2. A path to an output file, where results will be written.
+
+For example:
+
+```sh
+python optimize.py load.csv tmp/out.csv
+```
+
+The output file will contain entries for each hour:
+
+* `index` - The hour number; starts at 0.
+* `residual_load` - The original residual load.
+* `adjusted_load` - The adjusted load after the battery has charged or discharged.
+* `charge` - The amount of energy the battery charged (positive) or discharged (negative) in MW.
+* `soc` - The amount of energy stored in the battery in MWh.
+* `price` - The price of energy. Only included when optimizing for profit.
+
+Run the script with `-h` or `--help` for information on its arguments.
+
+## Setup
+
+### With Pipenv:
+
+```sh
+pipenv install
+```
+
+When using Pipenv, use optimize.py with `pipenv run python optimize.py ...`
+
+### Without Pipenv:
+
+Install Numpy:
+
+```sh
+pip install numpy
+```
+
+## Customizing the battery specifications
+
+The default battery has a volume of 50,000 MW (50 GW). This may be changed with the `--volume` or `-v` argument:
+
+```sh
+# Use a battery of  500 MW
+python optimize.py --volume 500 load.csv tmp/out.csv
+
+# Use a battery of 10,000 MW (10 GW)
+python optimize.py --volume 10000 load.csv tmp/out.csv
+```
+
+The battery will always have a capacity one-tenth of the volume: a battery of 50 GW volume will have 5 GW capacity. Change this with the `--capacity` or `-c` argument:
+
+```sh
+# Battery defaults to volume / 10 (5,000 MW)
+python optimize.py load.csv tmp/out.csv
+
+# Use a custom capacity of 2,500 MW
+python optimize.py --capacity 2500 load.csv tmp/out.csv
+```
+
+Both `--volume` and `--capacity` may be used together.
+
+```sh
+# Use a volume of 20,000 MW and capacity of 2,500 MW
+python optimize.py --capacity 2500 --volume 20000 load.csv tmp/out.csv
+```
+
+## Constraints
+
+When performing load flattening, the script calculates the 72-hour moving average for each hour in the load curve and attempts to move the load towards that mean.
+
+You may specify a custom constraint curve with the `--constrain` option. This CSV file should contain values in MW where negative values allow the battery to charge up to that capacity, and positive values allow it to discharge. The battery may not charge and discharge in the same hour.
+
+For example:
+
+```sh
+head constraint.csv
+# 2100.0    # => discharge up to 2100 GW
+# 1200.0    # => discharge up to 1200 GW
+# 200.0     # => discharge up to 200 MW
+# -800.0    # => charge up to 800 MW
+# -1800.0   # => charge up to 1800 MW
+# -2600.0   # => charge up to 2600 MW
+
+python optimize.py --constrain constraint.csv load.csv tmp/out.csv
+```
+
+## Gradual load flattening
+
+The `--gradual` option adds a step when load flattening. The assignment of energy is normally limited by the battery capacity and the constraint curve but may be further limited by the difference between the load in the minimum and maximum hour.
+
+For example, if the load in the peak hour is 10 GW, and the load in the trough is 6 GW, with a charging/discharging constraint of 2 GW, the algorithm would assign a 2 GW charge and 2 GW discharge in the min and max hours respectively.
+
+With gradual flattening, the script calculates the difference between the two loads (4 GW) and assigns only 5% of this (0.2 GW) and then adds the maximum hour back into the list of candidates for optimization, perhaps to be visited again in a future iteration.
+
+This gives hours around the ultimate peak the opportunity to also be flattened. Without gradual flattening, relatively small batteries are ineffective at peak shaving, and will often use all their volume on a single hour.
+
+This option significantly increases the calculation time, and cannot be used when optimizing for profit.
+
+## Optimizing for profit
+
+Instead of flattening a residual load curve, you may seek to maximize the profit of the battery; charging in the hours when energy is cheapest, and discharging when it is most expensive. This is accomplished by providing a price curve with the `--price` argument.
+
+```sh
+head price.csv
+# 24.52
+# 24.52
+# 29.96
+# 32.68
+
+python optimize.py --price price.csv load.csv tmp/out.csv
+```
+
+## Customizing the window (look behind) period
+
+The algorithm works by finding the hour with the largest peak - where the battery will discharge - and searches for the hour with the lowest trough - where the battery will charge. By default, the trough must occur within 72-hours prior to the peak (the "window").
+
+This has the effect that whenever the battery charges, it must discharge for the same amount within the window period.
+
+Smaller windows result in faster calculations and work well for small batteries. Higher periods will allow large batteries to be more effective but increase calculation time.
+
+## Example files
+
+The repository contains a couple of example files:
+
+* `constraint.csv` - A sinusoidal curve which serves as an example of a capacity constraint.
+* `load.csv` - A residual load curve from a default NL 2050 scenario.
+* `load.iinat.csv` - A residual load curve from the II3050 National Governance scenario.
+* `price.csv` - A price curve from a default NL 2050 scenario.
+* `price.iinat.csv` - A price from the the II3050 National Governance scenario.
---
+++ optimize.py
@@ -0,0 +1,308 @@
+from bisect import insort_left
+import argparse
+import numpy as np
+
+
+class Frame:
+    def __init__(self, index, value):
+        self.index = index
+        self.value = value
+        self.assigned = 0.0
+
+    def __lt__(self, other):
+        if self.value == other.value:
+            return self.index > other.index
+
+        return self.value < other.value
+
+    def assign(self, amount):
+        self.value += amount
+        self.assigned += abs(amount)
+
+
+class PriceFrame(Frame):
+    def assign(self, amount):
+        self.assigned += abs(amount)
+
+
+def read_curve(path):
+    with open(path) as file:
+        data = file.readlines()
+        data = [float(datum.rstrip()) for datum in data]
+
+    return data
+
+
+def append(list, item):
+    """Can be used instead of insort_left when optimizing with price curves."""
+    list.append(item)
+
+
+def mean_curve(values, samples=72):
+    """
+    Creates a curve containing the moving average of each point in the original. The default 72
+    samples means that the previous 36 hours and following 35 hours will be used to generate the
+    moving average for the current hour.
+    """
+    margin = int(samples / 2)
+    wrapped = values[len(values) - margin :] + values + values[:margin]
+
+    return np.resize(
+        np.convolve(wrapped, np.ones(samples), "valid") / samples,
+        len(values),
+    )
+
+
+def target_curves(load_curve, mean_curve):
+    """
+    Given the load curve and moving average mean curve, returns two new curves describing the amount
+    of charging or discharging which would be needed in each hour for a new curve to match the mean.
+    """
+    deviation_curve = np.array(load_curve) - np.array(mean_curve)
+
+    charging_target = [-value if value < 0.0 else 0.0 for value in deviation_curve]
+    discharging_target = [value if value > 0.0 else 0.0 for value in deviation_curve]
+
+    return (charging_target, discharging_target)
+
+
+def optimize(
+    data,
+    charging_target,
+    discharging_target,
+    capacity=5000.0,
+    gradual=False,
+    lookbehind=72,
+    price_curve=None,
+    volume=50000.0,
+):
+    """
+    Runs the optimization. Returns the energy stored in the battery in each hour.
+
+    Arguments:
+    data               - The residual load curve
+    charging_target    - Curve describing the desired charging in each hour
+    discharging_target - Curve describing the desired discharging in each hour
+
+    Keyword arguments:
+    volume      - The volume of the battery in MWh.
+    capacity    - The volume of the battery in MW.
+    lookbehind  - How many hours the algorithm can look into the past to search for the minimum.
+    price_curve - An optional price curve. If given, the algorithm will optimize for profit using
+                  the price curve rather than flattening the load curve.
+    """
+    optimize_profit = price_curve != None
+
+    insort_max = append if optimize_profit else insort_left
+
+    # All values for the year converted to a Frame.
+    if optimize_profit:
+        frames = [PriceFrame(index, value) for (index, value) in enumerate(price_curve)]
+    else:
+        frames = [Frame(index, value) for (index, value) in enumerate(data)]
+
+    # Contains all hours where there is room to discharge, sorted in ascending order (highest value
+    # is last).
+    charge_frames = sorted(
+        [frame for frame in frames if discharging_target[frame.index] > 0]
+    )
+
+    # Keeps track of how much energy is in the reserve in each hour.
+    reserve = np.zeros(len(data))
+
+    # Keeps track of how much energy has been assigned in each hour.
+    # assigned = np.zeros(len(data))
+
+    # Convert the charging and discharging targets to ndarray, constrained by the capacity of the
+    # battery.
+    charging_target = np.array([min(capacity, value) for value in charging_target])
+    discharging_target = np.array(
+        [min(capacity, value) for value in discharging_target]
+    )
+
+    while len(charge_frames) > 0:
+        max_frame = charge_frames.pop()
+
+        # Eventually contains the amount of energy to be charged at the min and discharged at the
+        # max frames.
+        available_energy = discharging_target[max_frame.index]
+
+        # The frame cannot be discharged any further (no margin between current and target load or
+        # the battery is already at max capacity).
+        if available_energy == 0:
+            continue
+
+        # Only charge from an hour whose value is 95% or lower than the max.
+        desired_low = max_frame.value * 0.95
+
+        # Contains the hour within the lookbehind periods with the minimum value.
+        min_frame = None
+
+        for min_index in range(
+            max_frame.index - 1, min(0, max_frame.index - lookbehind) - 1, -1
+        ):
+            if reserve[min_index] >= volume:
+                # We've reached a frame already at max-capacity; therefore neither it nor an earlier
+                # frame will be able to charge.
+                break
+
+            current = frames[min_index]
+
+            # Limit charging by the remaining volume in the frame.
+            available_energy = min(available_energy, volume - reserve[min_index])
+
+            if (
+                available_energy > 0
+                and charging_target[current.index] > 0
+                and (not min_frame or current.value < min_frame.value)
+                and current.value < desired_low
+            ):
+                min_frame = current
+
+        # We now have either the min frame, or None in which case no optimisation can be performed
+        # on the max frame.
+        if min_frame == None:
+            continue
+
+        # Contrain the charge/discharge by the charging target.
+        available_energy = min(available_energy, charging_target[min_frame.index])
+
+        if gradual and not optimize_profit:
+            # Take the half-way point between the peak and trough, if possible.
+            upper = max_frame.value
+            lower = min_frame.value
+
+            # Restrict the amount of energy assigned to be one twentieth of the difference between
+            # the max and min. This allows energy to be assigned more fairly to surrounding hours in
+            # later iterations.
+            available_energy = min(available_energy, (upper - lower) / 20)
+
+        if available_energy == 0:
+            continue
+
+        # Add the charge and discharge to the reserve.
+        reserve[(min_frame.index) : (max_frame.index)] += available_energy
+
+        min_frame.assign(available_energy)
+        max_frame.assign(-available_energy)
+
+        charging_target[min_frame.index] -= available_energy
+        discharging_target[max_frame.index] -= available_energy
+
+        if discharging_target[max_frame.index] > 0:
+            insort_max(charge_frames, max_frame)
+
+    return reserve
+
+
+def build_targets(args, loads, capacity):
+    if args.constraints_path:
+        charge = np.zeros(len(loads))
+        discharge = np.zeros(len(loads))
+
+        for (index, value) in enumerate(read_curve(args.constraints_path)):
+            if value < 0:
+                discharge[index] = abs(value)
+            else:
+                charge[index] = abs(value)
+
+        return (charge, discharge)
+
+    if args.price_path:
+        return (np.repeat(capacity, len(loads)), np.repeat(capacity, len(loads)))
+
+    # If we are still missing a curve, it will be based on the moving average of the load.
+
+    return target_curves(loads, mean_curve(loads))
+
+
+def run(args):
+    """
+    Runs the optimization using the args provided on the command-line.
+    """
+    capacity = args.capacity or args.volume / 10
+    loads = read_curve(args.input_path)
+    prices = read_curve(args.price_path) if args.price_path else None
+
+    (charging_target, discharging_target) = build_targets(args, loads, capacity)
+
+    reserve = optimize(
+        loads,
+        charging_target,
+        discharging_target,
+        capacity=capacity,
+        gradual=args.gradual,
+        lookbehind=args.window,
+        price_curve=prices,
+        volume=args.volume,
+    )
+
+    with open(args.output_path, "w") as file:
+        if prices == None:
+            file.write("index,residual_load,adjusted_load,charge,soc\n")
+        else:
+            file.write("index,residual_load,adjusted_load,charge,soc,price\n")
+
+        prev_load = 0.0
+        for index in range(0, len(loads)):
+            charge = reserve[index] - prev_load
+
+            file.write(str(index))
+            file.write(",")
+            file.write(str(loads[index]))
+            file.write(",")
+            file.write(str(loads[index] + charge))
+            file.write(",")
+            file.write(str(charge))
+            file.write(",")
+            file.write(str(reserve[index]))
+            if prices != None:
+                file.write(",")
+                file.write(str(prices[index]))
+            file.write("\n")
+
+            prev_load = reserve[index]
+
+
+parser = argparse.ArgumentParser(
+    description="Flatten a residual load curve using a battery"
+)
+parser.add_argument("input_path", help="Path to residual load CSV file.")
+parser.add_argument("output_path", help="Path to output file.")
+parser.add_argument(
+    "-c",
+    "--capacity",
+    type=float,
+    help="The battery capacity in MW; defaults to 10 percent of the volume",
+)
+parser.add_argument(
+    "--gradual",
+    action="store_true",
+    help="Use an iterative process to flatten the curve gradually (this is slow)",
+)
+parser.add_argument(
+    "--price",
+    dest="price_path",
+    help="Path to an optional price curve; uses profit optimization instead of load flattening",
+)
+parser.add_argument(
+    "--constrain",
+    dest="constraints_path",
+    help="Path to an optional constraint CSV",
+)
+parser.add_argument(
+    "-v",
+    "--volume",
+    default=50000.0,
+    type=float,
+    help="The battery volume in MWh; defaults to 50_000",
+)
+parser.add_argument(
+    "-w",
+    "--window",
+    default=72,
+    type=int,
+    help="The number of hours after charging when energy must be discharged; defaults to 72",
+)
+
+run(parser.parse_args())
